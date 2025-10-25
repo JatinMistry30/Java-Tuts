82a21193a32d687f4f3c91507ba56c7e106bd909
jatin9975 <jatinmistry9975@gmail.com>
2025-10-25 12:11:30 +0530

    Code Added to github

---
+++ 25October2025/ArrayExample.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class ArrayExample {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int[] arr = new int[5];
+
+        for(int i =0; i < arr.length; i++){
+            arr[i] = in.nextInt();
+        }
+        for(int i =0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+        in.close();
+    }    
+}
