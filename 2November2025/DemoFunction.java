bb99fb1518f654c7dd08857b23783ca20a809951
jatin9975 <jatinmistry9975@gmail.com>
2025-11-02 09:16:12 +0530

    Added code to github

---
+++ 2November2025/DemoFunction.java
@@ -0,0 +1,10 @@
+
+public class DemoFunction {
+    static int myMethod(String fname) {
+        System.out.println("This is the function "+ fname);
+        return 0;
+    }
+    public static void main(String[] args) {
+        myMethod("jatin");
+    }    
+}
